e9629694546bcc8d6e398171763abb298409e3e0
Gabriel Brizola <gabrielbrizola13@gmail.com>
2025-04-29 12:57:17 -0300

    fix: integration db

--- Back-end/app/config/database.py
+++ Back-end/app/config/database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 import os # Importando o dotenv para carregar variáveis de ambiente
+
+load_dotenv()
 
 # Configurações do banco de dados
 DB_USER = os.getenv("DB_USER")
