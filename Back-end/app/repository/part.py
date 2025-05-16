0a9136b1cd6f20f3165b7203e54955036f0dedb6
GabrielPerosa <gabrielperosa493@gmail.com>
2025-05-16 12:54:29 -0300

    fix: adjusting part repository

--- Back-end/app/repository/part.py
+++ Back-end/app/repository/part.py
@@ -2,23 +2,22 @@ import uuid
 from sqlalchemy.orm import Session
 from app.models.part import PartDB
 from app.schemas.dto.part import PartDTO
-from app.schemas.part import Part
 
-def create_part(db: Session, part_dto: PartDTO) -> Part:
+def create_part(db: Session, part_dto: PartDTO) -> PartDB:
     part = PartDB(
         id=str(uuid.uuid4()),
         type=part_dto.type,
-        cycle_id=part_dto.cycle_id
     )
     db.add(part)
     db.commit()
     db.refresh(part)
     return part
 
-def get_all_parts(db: Session) -> list[Part]:
+def get_all_parts(db: Session) -> list[PartDB]:
     return db.query(PartDB).all()
 
-def get_quantity_parts(db: Session) -> int:
+"""
+ def get_quantity_parts(db: Session) -> int:
     return db.query(PartDB).count()
 
 def get_parts_by_type(db: Session, type: str) -> Part | None:
@@ -26,3 +25,4 @@ def get_parts_by_type(db: Session, type: str) -> Part | None:
 
 def get_quantity_parts_by_type(db: Session, type: str) -> int:
     return db.query(PartDB).filter(PartDB.type == type).count()
+"""
