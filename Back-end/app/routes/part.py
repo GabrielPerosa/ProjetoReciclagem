4311932c8e61aca2efcae630c20dac3a12f0ed17
GabrielPerosa <gabrielperosa493@gmail.com>
2025-05-19 15:49:12 -0300

    fix: adjusting routes

--- Back-end/app/routes/part.py
+++ Back-end/app/routes/part.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from typing import List
 from app.schemas.part import Part
 from app.schemas.dto.part import PartDTO
 from app.repository import part as part_repository
@@ -7,7 +8,11 @@ from app.config.database import get_db
 
 router = APIRouter(prefix="/parts", tags=["Parts"])
 
-@router.get("/", response_model=list[Part])
+@router.post("/", response_model=Part)
+def create_part(part: PartDTO, db: Session = Depends(get_db)):
+    return part_repository.create_part(db, part)
+
+@router.get("/", response_model=List[Part])
 def list_all(db: Session = Depends(get_db)):
     return part_repository.get_all_parts(db)
 
@@ -19,6 +24,6 @@ def get_quantity(db: Session = Depends(get_db)):
 def get_part_by_type(type: str, db: Session = Depends(get_db)):
     return part_repository.get_parts_by_type(db, type)
 
-@router.get("/quantity/{type}", response_model=int)
+@router.get("/type/{type}/quantity", response_model=int)
 def get_quantity_by_type(type: str, db: Session = Depends(get_db)):
     return part_repository.get_quantity_parts_by_type(db, type)
