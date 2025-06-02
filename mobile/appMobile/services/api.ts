12fda9e3a2f83b3d4f70bfd9d0729efbe4fb5fd0
Gabriel Brizola <gabrielbrizola13@gmail.com>
2025-06-02 19:12:36 -0300

    chore: update API baseURL in Axios

--- mobile/appMobile/services/api.ts
+++ mobile/appMobile/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://ncr19qt9xd.execute-api.us-east-1.amazonaws.com',
+  baseURL: 'https://ydsdpxj25l.execute-api.us-east-1.amazonaws.com',
   headers: {
     'Content-Type': 'application/json',
   },
