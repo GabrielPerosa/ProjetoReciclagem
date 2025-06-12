8254f7b52409ec9846c5f22d421ecd3822beee37
Gabriel Brizola <158374028+GBrizola@users.noreply.github.com>
2025-06-12 11:56:25 -0300

    fix: app

--- Back-end/app/main.py
+++ Back-end/app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.routes.router import router
 from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -10,7 +12,6 @@ app.add_middleware(
     allow_headers=["*"], 
 )
 
-app = FastAPI()
 app.include_router(router)
 
 @app.get("/")
